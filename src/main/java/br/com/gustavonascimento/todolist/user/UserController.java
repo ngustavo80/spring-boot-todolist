40fd05408aabf749c6e98fab672859230e4c8c6d
ngustavo80 <ngustavo80@hotmail.com>
2023-10-10 21:46:46 -0300

    first commit

---
+++ src/main/java/br/com/gustavonascimento/todolist/user/UserController.java
@@ -0,0 +1,35 @@
+package br.com.gustavonascimento.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+  
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel);
+    }
+}
+
+/*
+ * Tipos de dados
+ * String
+ * Integer (int)
+ * Double (números casas decimais)
+ * Float (também números com casas decimais, só diferencia a quantidade de números aceitos)
+ * char (caracteres)
+ * Date (data)
+ * void (quando queremos que o método nãop tenha nenhum retorno)
+ * Etc
+ */
+
+/*
+ * Modificadores
+ * public
+ * private
+ * protected
+ */
---
+++ src/main/java/br/com/gustavonascimento/todolist/user/UserModel.java
@@ -0,0 +1,8 @@
+package br.com.gustavonascimento.todolist.user;
+
+public class UserModel {
+  
+    private String username;
+    private String name;
+    private String password;
+}
